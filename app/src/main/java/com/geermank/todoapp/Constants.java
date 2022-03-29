0ec4e18b1a135de9735d0488716d6a8dc824aaa7
german <germankrajcar@gmail.com>
2022-03-28 21:59:06 -0300

    Clase 7

---
+++ app/src/main/java/com/geermank/todoapp/Constants.java
@@ -0,0 +1,7 @@
+package com.geermank.todoapp;
+
+public class Constants {
+    public static final String USER_SETTINGS_FILE_NAME = "USER_SETTINGS_FILE_NAME";
+    public static final String USER_EMAIL = "USER_EMAIL";
+    public static final String USER_PASS = "USER_PASS";
+}
--- app/src/main/java/com/geermank/todoapp/LoginActivity.java
+++ app/src/main/java/com/geermank/todoapp/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.geermank.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,26 +13,34 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.geermank.todoapp.main.MainActivity;
+import com.geermank.todoapp.settings.UserCredentials;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvWelcomeMessage, tvTermsAndConditions;
     private EditText etEmail, etPassword;
     private Button btnLogin;
+    private AppCompatCheckBox cbRememberCredentials;
+
+    private UserCredentials userCredentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        userCredentials = new UserCredentials(this);
 
         tvWelcomeMessage = findViewById(R.id.tv_welcome_message);
         tvTermsAndConditions = findViewById(R.id.tv_terms_and_conditions);
         etEmail = findViewById(R.id.et_email);
         etPassword = findViewById(R.id.et_password);
         btnLogin = findViewById(R.id.btn_login);
+        cbRememberCredentials = findViewById(R.id.cb_remember_credentials);
 
         btnLogin.setOnClickListener(this);
         tvTermsAndConditions.setOnClickListener(this);
+
+        checkIfUserHadSavedCredentials();
     }
 
     @Override
@@ -41,7 +52,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    private void checkIfUserHadSavedCredentials() {
+        if (userCredentials.isUserLogged()) {
+            navigateToMainActivity();
+        }
+    }
+
     private void login() {
+        saveUserCredentials();
+        navigateToMainActivity();
+    }
+
+    private void saveUserCredentials() {
+        if (!cbRememberCredentials.isChecked()) {
+            return;
+        }
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        userCredentials.saveUserCredentials(email, password);
+    }
+
+    private void navigateToMainActivity() {
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
         finish();
